9e8b9bc9f870c8f03fb22474cbc52220e92ea8dd
suchitrawat <rawats@student.unimelb.edu.au>
2024-06-09 11:21:53 +1000

    first commit

---
+++ movies/src/test/java/unimelb/rawats/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package unimelb.rawats.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
